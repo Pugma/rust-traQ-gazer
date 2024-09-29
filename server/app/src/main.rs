8d5cb9a09ae31cfe7253081af07d775571155b40
Pugma <maronmarondelicious@gmail.com>
2024-09-29 16:26:40 +0900

    axum serve in thread / added logging system

--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -3,23 +3,30 @@ mod handler;
 mod repo;
 mod traq;
 
+use log::info;
+
 use handler::Handler;
 use repo::Repository;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, spawn};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // enable env_logger
+    env_logger::init();
+
     // setup DB connection
     let repo = Repository::setup().await.expect("Failed to access the DB!");
-    println!("Made connections to the DB correctly!");
+    info!("Made connections to the DB correctly!");
 
     // setup API server
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     let app = openapi::server::new(Handler { repo });
-    axum::serve(listener, app)
-        .await
-        .expect("Failed to open the endpoints!");
-    println!("Opened the endpoints correctly!");
+    spawn(async move {
+        println!("Opening the endpoints ...");
+        axum::serve(listener, app)
+            .await
+            .expect("Failed to open the endpoints!");
+    });
 
     // setup message poller
     traq::start_polling().await?;
