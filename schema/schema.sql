e159983278192515b22fe09c0d26ec7945da473c
Pugma <maronmarondelicious@gmail.com>
2024-10-26 00:23:55 +0900

    changed excluding table

--- schema/schema.sql
+++ schema/schema.sql
@@ -16,10 +16,27 @@ CREATE TABLE IF NOT EXISTS `words` (
   FOREIGN KEY (`trap_id`) REFERENCES `users`(`trap_id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `excluded_users` (
-  `word_id` BIGINT(8) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `word_excluded_users` (
+  `word_id` BIGINT(8) NOT NULL,
   `trap_id` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (`word_id`),
+  FOREIGN KEY (`word_id`) REFERENCES `words`(`word_id`),
+  FOREIGN KEY (`trap_id`) REFERENCES `users`(`trap_id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `stamps` (
+  `stamp_id` BIGINT(8) NOT NULL AUTO_INCREMENT,
+  `stamp_uuid` BINARY(36) NOT NULL,
+  `trap_id` VARCHAR(32) NOT NULL,
+  `word` VARCHAR(50) NOT NULL,
+  `register_time` DATETIME DEFAULT NOW(),
+  PRIMARY KEY (`stamp_id`),
+  FOREIGN KEY (`trap_id`) REFERENCES `users`(`trap_id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `stamp_excluded_users` (
+  `stamp_id` BIGINT(8) NOT NULL,
+  `trap_id` VARCHAR(32) NOT NULL,
+  FOREIGN KEY (`stamp_id`) REFERENCES `stamps`(`stamp_id`),
   FOREIGN KEY (`trap_id`) REFERENCES `users`(`trap_id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
