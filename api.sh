dd01cbc69d05e5206d2bbc3ba2bbed561370302c
Pugma <maronmarondelicious@gmail.com>
2025-04-22 14:51:26 +0900

    refactor: moved `openapi.yaml`

--- api.sh
+++ api.sh
@@ -2,6 +2,6 @@ docker run --rm -v "${PWD}:/local" -u $(id -u) \
 openapitools/openapi-generator-cli:v7.12.0 \
 generate \
 -g rust-axum \
--i /local/docs/openapi.yaml \
+-i /local/schema/openapi.yaml \
 -o /local/server/apis \
 --generate-alias-as-model
