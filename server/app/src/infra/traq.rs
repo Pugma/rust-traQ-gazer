67e013b54657cf3b15ab59b7743dcdac57ef9bf0
Pugma <maronmarondelicious@gmail.com>
2025-08-13 15:46:03 +0900

    chore: deleted unused file & imports

--- server/app/src/infra/traq.rs
+++ server/app/src/infra/traq.rs
@@ -1,13 +1,2 @@
-use std::{env, sync::LazyLock};
-use traq::apis::configuration::Configuration;
-
-pub mod message;
 pub mod message_poller_impl;
 pub mod notification;
-
-static CONFIG: LazyLock<Configuration> = LazyLock::new(|| Configuration {
-    bearer_access_token: Some(
-        env::var("BOT_ACCESS_TOKEN").expect("Couldn't find BOT_ACCESS_TOKEN"),
-    ),
-    ..Default::default()
-});
