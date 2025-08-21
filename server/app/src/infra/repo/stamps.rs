6585ca596ef0eabccbc43af1e56f07eaf3c87001
Pugma <maronmarondelicious@gmail.com>
2025-08-21 21:41:48 +0900

    chore(repo): add StampRepository impl stubs

--- server/app/src/infra/repo/stamps.rs
+++ server/app/src/infra/repo/stamps.rs
@@ -1,1 +1,25 @@
+use crate::{
+    domain::{
+        stamp::{NewStamp, Stamp, StampId, StampRepository},
+        user::UserId,
+    },
+    infra::repo::Repository,
+};
 
+impl StampRepository for Repository {
+    async fn register(&self, stamp: NewStamp) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    async fn find_all(&self) -> Result<Vec<Stamp>, String> {
+        unimplemented!()
+    }
+
+    async fn find_by_user_id(&self, user_id: &UserId) -> Result<Vec<Stamp>, String> {
+        unimplemented!()
+    }
+
+    async fn delete(&self, stamp_id: &StampId) -> Result<(), String> {
+        unimplemented!()
+    }
+}
